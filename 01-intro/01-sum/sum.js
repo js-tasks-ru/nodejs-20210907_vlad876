c163bc633d3f1882c160be8d11d569710d3f2eb0
Vladyslav Shyrinov <vladyslav.shyrinov@jameda.de>
2021-09-09 21:09:48 +0200

    Implemented sum function for numbers with type check

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a+b;
+  }
+
+  throw new TypeError('Arguments must be numbers');
 }
 
 module.exports = sum;
