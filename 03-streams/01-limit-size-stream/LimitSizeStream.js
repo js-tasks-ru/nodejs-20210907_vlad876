d18ea22553b7c0f2e2070a30c8341b0e12a2ba62
Vladyslav Shyrinov <vladyslav.shyrinov@jameda.de>
2021-09-16 13:04:00 +0200

    Implemented tasks with streams

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.currentByteLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    if (this.currentByteLength + chunk.byteLength > this.limit) {
+      callback(new LimitExceededError());
+    }
 
+    this.currentByteLength += chunk.byteLength;
+    callback(null, chunk);
   }
 }
 
