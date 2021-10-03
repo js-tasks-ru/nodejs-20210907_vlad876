f7027c7bca6169d1e5689ff541ee09316ab4ad25
Vladyslav Shyrinov <vladyslav.shyrinov@jameda.de>
2021-10-03 13:01:09 +0200

    Implemented categories + products api creation for mongodb/mongoose task

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,52 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  let products = await Product.find({subcategory});
+
+  products = products.map(({_doc: {_id, __v, ...rest}}) => ({
+    id: _id,
+    ...rest,
+  }));
+
+  ctx.body = {products};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  let products = await Product.find({});
+
+  products = products.map(({_doc: {_id, __v, ...rest}}) => ({
+    id: _id,
+    ...rest,
+  }));
+
+  ctx.body = {products};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const objectId = ctx.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(objectId)) {
+    ctx.throw(400);
+  } else {
+    const product = await Product.findById(ctx.params.id);
+
+    if (!product) {
+      ctx.throw(404, 'Not found');
+    } else {
+      const {_doc: {_id, __v, ...rest}} = product;
+
+      ctx.body = {
+        product: {
+          id: _id,
+          ...rest,
+        }
+      };
+    }
+  }
 };
 
